7d4b72ce0db840ce741104e143a4a21450971de6
elnaz-24 <elnaz.khani24@gmail.com>
2024-11-22 17:01:26 +0300

    Add delete button functionality in details

--- src/App.js
+++ src/App.js
@@ -59,9 +59,24 @@ function App() {
       };
     });
   }
+  // cancel button for details
+  function handleDeleteProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  }
+
   // console.log(projectsState);
-const selectedProject=projectsState.projects.find(project=>project.id===projectsState.selectedProjectId)
-  let content=<SelectedProject project={selectedProject}/>;
+  const selectedProject = projectsState.projects.find(
+    (project) => project.id === projectsState.selectedProjectId
+  );
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
   if (projectsState.selectedProjectId === null) {
     content = (
       <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
